a14fe0549870cfcf34656bea7a3c66c144be9185
ThiagoOkamura <thiagokamura14@gmail.com>
2025-09-03 11:27:14 -0400

    Add files via upload

---
+++ CourseGrades.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author 2279654
+ */
+public class CourseGrades {
+    private GradedActivity[] grades = new GradedActivity[4];
+
+    public CourseGrades() {
+    }
+    
+    public void setLab(GradedActivity aLab) {
+        grades[0] = aLab;
+    }
+    
+    public void setPassFailExam(PassFailExam aPassFailExam) {
+        grades[1] = aPassFailExam;
+    }
+    
+    public void setEssay(Essay anEssay) {
+        grades[2] = anEssay;
+    }
+    
+    public void setFinalExam(FinalExam aFinalExam) {
+        grades[3] = aFinalExam;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseGrades{" + "grades=" + grades + '}';
+    }
+}

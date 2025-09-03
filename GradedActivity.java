ff3273d61a5d9ba8f6037d22a9e7bf525d257a94
ThiagoOkamura <thiagokamura14@gmail.com>
2025-09-03 10:19:58 -0400

    Update GradedActivity.java

--- GradedActivity.java
+++ GradedActivity.java
@@ -9,5 +9,27 @@ package assignment1;
  * @author thiag
  */
 public class GradedActivity {
+    private double score;
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
     
+    public char getGrade() {
+        int intscore =  (int) score;
+        switch (intscore / 10) {
+            case 10: return 'A'; 
+            case 9: return 'A'; 
+            case 8: return 'B'; 
+            case 7: return 'C'; 
+            case 6: return 'D'; 
+            default: return 'F'; 
+        }
+    }
 }
+
+
